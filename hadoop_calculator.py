25b3cc79732708d37c33a197c8aa6b7abbc0ccff
Jon Miller <jonEbird@gmail.com>
2009-07-26 18:14:03 -0400

    much improved version alowing to solve for anything

--- hadoop_calculator.py
+++ hadoop_calculator.py
@@ -12,7 +12,7 @@ def filesize(size):
         if factor in scalar:
             return digits * 1024**scalar.index(factor)
         else:
-            return 0 #digits
+            return digits
     except (ValueError), e:
         return 0
 
@@ -33,66 +33,70 @@ def humantime(s):
     """seconds to a readable human format"""
     return str(datetime.timedelta(seconds=int(s)))
 
-def hadoopcalc(gigs=0, mbps=0, nodes=0, seconds=0):
+def hadoopcalc(megs=0, mbps=0, nodes=0, seconds=0):
     """Summarized Equation := (gigs * 1024) * 8 / (mbps * nodes) = seconds
                            :=  seconds * (mbps * nodes) / 8192   = gigs
 		           :=  gigs * 8192 / seconds / mbps      = nodes
 		           :=  gigs * 8192 / seconds / nodes     = mbps"""
-    if (mbps and nodes and seconds) and not gigs:
-        # Solve for gigs
-        return seconds * (mbps * nodes) / 8192
-    elif (gigs and nodes and seconds) and not mbps:
-        # Calculate aggregate throughput
-        return gigs * 8192 / seconds / nodes
-    elif (gigs and mbps and seconds) and not nodes:
-        # Return number of nodes required
-        return gigs * 8192 / seconds / mbps
-    elif (gigs and mbps and nodes) and not seconds:
-        # see how long it takes to process
-        return (gigs * 8192) / (mbps * nodes)
+    if (mbps and nodes and seconds) and not megs:
+        return seconds * (mbps * nodes) * 1024 / 8192  #= megs
+    elif (megs and nodes and seconds) and not mbps:
+        return megs * 8 / seconds / nodes              #= aggregate throughput (mbps)
+    elif (megs and mbps and seconds) and not nodes:
+        return megs * 8 / seconds / mbps               #= nodes
+    elif (megs and mbps and nodes) and not seconds:
+        return megs * 8 / mbps / nodes                 #= seconds
     else:
-        raise Exception, "Need to specify 3 of the 4 parameters of gigs, mbps, nodes or seconds. No more, no less."
+        raise Exception, "Need to specify 3 of the 4 parameters of megs, mbps, nodes or seconds. No more, no less."
 
 if __name__ == '__main__':
     
     from optparse import OptionParser
-    USAGE = """Usage: %s <options>
-       filesizes can be in exa(e), peta(p), tera(t), gig(g), mega(m), kilo(k).
-         E.g. 2t or 250g
-       Or you may specify time units and you will then get estimates on how much data you can churn in that timeframe.
-       Please abbreviate common units to a single letters: second=s, minute=m, hour=h, day=d, week=w
-         E.g. 2h or 2d""" % \
-            (os.path.basename(sys.argv[0]))
+    USAGE = """Usage: %prog <options> gigs|mbps|nodes|seconds
+       Solve for one of the four variables while specifying the other three."""
+
     parser = OptionParser(usage=USAGE)
+    parser.add_option("-d", "--data", dest="data", default="100g",
+                      help="Amount of data we're talking about. Filesizes can be in exa(e), peta(p), tera(t), gig(g), mega(m), kilo(k)")
+    parser.add_option("-m", "--mbps", dest="mbps", default=150,
+                      help="aggregate throughput for cluster processing in Mbps")
     parser.add_option("-n", "--nodes", dest="nodes", default=10,
                       help="number of nodes in your cluster")
-    parser.add_option("-c", "--copies", dest="copies", default=3,
-                      help="number of copies made per file in your HDFS")
-    parser.add_option("-m", "--mbps", dest="mbps", default=1000,
-                      help="network speed measured in Mbps")
-    parser.add_option("-t", "--throughput", dest="tput", default=150,
-                      help="aggregate throughput for cluster processing in Mbps")
+    parser.add_option("-t", "--time", dest="time", default="30m",
+                      help="How much time are you giving us? Time can be in second(s), minute(m), hour(h), day(d), week(w)")
+    parser.add_option("-q", "--quick", dest="quick", action="store_true", default=False,
+                      help="Quickly and quietly, just give me the number without the fluff")
 
     (opt, args) = parser.parse_args()
 
-    total_mb = sum(map(filesize, args))
-    total_s  = sum(map(unitoftime, args))
-    print 'DEBUG: total_mb = %d, total_s = %d' % (total_mb, total_s)
-    
-    if total_mb:
-        print 'In theory, you should be able to transfer your %.2f Gigs of data in %s' % \
-            (total_mb / 1024.0, humantime(total_mb / int(opt.nodes) * int(opt.copies) * 8 / int(opt.mbps)))
+    if not args or not args[0] in ['gigs', 'mbps', 'nodes', 'seconds']:
+        parser.print_help()
+        sys.exit(1)
 
-        print '...but more importantly...'
+    # gather our defaults
+    defs = {
+        'megs': filesize(opt.data),
+        'mbps': int(opt.mbps),
+        'nodes': int(opt.nodes),
+        'seconds': unitoftime(opt.time) }
 
-        print 'Given your aggregate throughput of %s Mbps, representing cluster hardware and mapreduce job:\nYou should be able to process your %d Gigs of data in %s' \
-            % (opt.tput, total_mb / 1024, humantime(total_mb * 8 / (int(opt.tput) * int(opt.nodes))))
+    if args[0] == 'gigs':
+        defs['megs'] = hadoopcalc(mbps=defs['mbps'], nodes=defs['nodes'], seconds=defs['seconds'])
+    elif args[0] == 'mbps':
+        defs['mbps'] = hadoopcalc(megs=defs['megs'], nodes=defs['nodes'], seconds=defs['seconds'])
+    elif args[0] == 'nodes':
+        defs['nodes'] = hadoopcalc(megs=defs['megs'], mbps=defs['mbps'], seconds=defs['seconds'])
+    elif args[0] == 'seconds':
+        defs['seconds'] = hadoopcalc(megs=defs['megs'], mbps=defs['mbps'], nodes=defs['nodes'])
 
-    if total_s:
-        print 'In theory, you should be able to transfer %.2f Gigs in the specified time of %s' % \
-            (total_s * int(opt.mbps) * int(opt.nodes) / (int(opt.copies) * 8) / 1024, humantime(total_s))
+    defs['gigs'] = defs['megs'] / 1024
 
-        print '...but more importantly...'
-
-        print 'Given your aggregate throughput of %s Mbps, representing cluster hardware and mapreduce job:\nYou should be able to process %d Gigs of data in your specified %s amount of time.' \
-            % (opt.tput, total_s * int(opt.tput) * int(opt.nodes) / 8192, humantime(total_s))
+    if opt.quick:
+        print defs[args[0]]
+        sys.exit(0)
+    else:
+        print '%s = %d\nBreakdown:' % (args[0], defs[args[0]])
+        print '  %d Gigs of data' % (defs['gigs'])
+        print '  %d Mbps aggregate throughput' % (defs['mbps'])
+        print '  %d number of nodes in the cluster' % (defs['nodes'])
+        print '  %s amount of time to do it all...' % (humantime(defs['seconds']))
